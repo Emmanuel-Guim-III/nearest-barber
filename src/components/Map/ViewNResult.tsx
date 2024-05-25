e07082d367f81fff821310513038daa03a7545fe
Emmanuel Guim III <emmanuelguim.third@gmail.com>
2024-05-26 03:37:47 +0800

    Implemented a spring animation that enables "ViewNResult" component to be swiped up

--- src/components/Map/ViewNResult.tsx
+++ src/components/Map/ViewNResult.tsx
@@ -1,22 +1,50 @@
+import classNames from 'classnames';
+import { motion, useAnimationControls } from 'framer-motion';
+import { useEffect, useState } from 'react';
 import { useSwipeable } from 'react-swipeable';
 import { Worker } from '../Worker/WorkerView';
 
 type Props = { data: Worker[] };
 
+const VARIANTS = {
+  open: { y: -200, opacity: 0.5, scale: 0.8 },
+  closed: { y: 0, opacity: 1, scale: 1 },
+};
+
+const SPRING = {
+  type: 'spring',
+  stiffness: 700,
+  damping: 30,
+};
+
 export function ViewNResults({ data }: Props) {
+  const controls = useAnimationControls();
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    controls.start(isOpen ? 'open' : 'closed');
+  }, [controls, isOpen]);
+
   const handlers = useSwipeable({
-    onSwipedUp: () => console.log('up'),
+    onSwipedUp: () => setIsOpen(true),
+    onSwipedDown: () => setIsOpen(false),
     swipeDuration: 500,
     preventScrollOnSwipe: true,
     trackMouse: true,
   });
 
   return (
-    <button
+    <motion.button
       {...handlers}
-      className='absolute bottom-0 left-0 right-0 mx-auto w-fit !rounded-[0px] !rounded-t-xl bg-accent px-3 py-1 text-white shadow'
+      animate={controls}
+      transition={SPRING}
+      variants={VARIANTS}
+      className={classNames(
+        'absolute bottom-0 left-0 right-0 mx-auto w-fit bg-accent px-3 py-1 text-white shadow',
+        isOpen ? '!rounded-xl' : '!rounded-[0px] !rounded-t-xl',
+      )}
     >
-      VIEW {data.length} RESULTS
-    </button>
+      {isOpen ? 'SWIPE DOWN TO CLOSE' : `VIEW ${data.length} RESULTS`}
+    </motion.button>
   );
 }
