3bfbc5798f78c4e159a72bba1fd5682a6c214f7f
Emmanuel Guim III <emmanuelguim.third@gmail.com>
2024-06-03 02:05:41 +0800

    Resolved "unique key" warning in stars component

--- src/components/Worker/WorkerView.tsx
+++ src/components/Worker/WorkerView.tsx
@@ -1,29 +1,38 @@
 import { FaRegStar, FaStar, FaStarHalfAlt } from 'react-icons/fa';
+import { Fragment } from 'react/jsx-runtime';
 import { workers } from '../mockData';
 
 export type Worker = (typeof workers)[number];
 
-export function WorkerView({ data }: { data: Worker }) {
+type Props = { data: Worker };
+
+const Stars = ({ count }: { count: number }) => {
+  const stars = [];
+  const fullStars = Math.floor(count);
+  const hasHalfStar = count % 1 !== 0;
+
+  for (let i = 0; i < fullStars; i++) {
+    stars.push(<FaStar />);
+  }
+
+  if (hasHalfStar) stars.push(<FaStarHalfAlt />);
+
+  const totalStars = fullStars + (hasHalfStar ? 1 : 0);
+  for (let i = totalStars; i < 5; i++) {
+    stars.push(<FaRegStar />);
+  }
+
+  return (
+    <div className='flex gap-1 text-lg text-yellow-star'>
+      {stars.map((star, i) => (
+        <Fragment key={i}>{star}</Fragment>
+      ))}
+    </div>
+  );
+};
+
+export function WorkerView({ data }: Props) {
   const { firstName, lastName, jobsAccomplished, rating, image } = data;
-
-  const Stars = ({ count }: { count: number }) => {
-    const stars = [];
-    const fullStars = Math.floor(count);
-    const hasHalfStar = count % 1 !== 0;
-
-    for (let i = 0; i < fullStars; i++) {
-      stars.push(<FaStar />);
-    }
-
-    if (hasHalfStar) stars.push(<FaStarHalfAlt />);
-
-    const totalStars = fullStars + (hasHalfStar ? 1 : 0);
-    for (let i = totalStars; i < 5; i++) {
-      stars.push(<FaRegStar />);
-    }
-
-    return <div className='flex gap-1 text-lg text-yellow-star'>{stars}</div>;
-  };
 
   return (
     <div className='flex w-[150px] flex-col items-center gap-3 rounded bg-base p-3 shadow-md'>
