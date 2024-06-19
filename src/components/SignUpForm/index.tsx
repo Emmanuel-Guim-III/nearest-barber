68f1001bc7a7b999444c897ba8692d4e47b8a766
Emmanuel Guim III <emmanuelguim.third@gmail.com>
2024-06-19 21:27:27 +0800

    UI | Created a signup form with firstName, lastName and contactNumber fields

--- src/components/SignUpForm/index.tsx
+++ src/components/SignUpForm/index.tsx
@@ -1,6 +1,9 @@
+import { Form, Formik } from 'formik';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import * as Yup from 'yup';
 
+import { MyTextInput } from '../InputFields';
 import { UserType } from '../variables';
 import { UserTypeSelect } from './UserTypeSelect';
 
@@ -19,7 +22,63 @@ export function SignUpForm() {
   return (
     <>
       {userType ? (
-        <div className='flex w-full flex-col items-center gap-1'>Signup</div>
+        <div className='flex flex-col items-center gap-10'>
+          <h1 className='text-tertiary'>Sign up</h1>
+          <Formik
+            initialValues={{
+              firstName: '',
+              lastName: '',
+              contactNumber: '',
+            }}
+            validationSchema={Yup.object({
+              firstName: Yup.string()
+                .max(15, 'Must be 20 characters or less')
+                .required('Required'),
+              lastName: Yup.string()
+                .max(20, 'Must be 20 characters or less')
+                .required('Required'),
+              contactNumber: Yup.number()
+                .positive('Invalid contact number')
+                .required('Required'),
+            })}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+              }, 400);
+            }}
+          >
+            <Form className='flex flex-col gap-4'>
+              <MyTextInput
+                label='First Name'
+                name='firstName'
+                type='text'
+                placeholder='First Name'
+              />
+
+              <MyTextInput
+                label='Last Name'
+                name='lastName'
+                type='text'
+                placeholder='Last Name'
+              />
+
+              <MyTextInput
+                label='Contact Number'
+                name='contactNumber'
+                type='number'
+                placeholder='09*********'
+              />
+
+              <button
+                className='mt-7 rounded-full bg-brand-light text-2xl text-white'
+                type='submit'
+              >
+                Submit
+              </button>
+            </Form>
+          </Formik>
+        </div>
       ) : (
         <UserTypeSelect onSelect={handleUserTypeSelect} />
       )}
